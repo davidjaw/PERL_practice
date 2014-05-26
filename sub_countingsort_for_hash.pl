2be9baff9e16d1abe9f3d1719f8b5645621f84df
David Jaw <gurinneko001@yahoo.com.tw>
2014-05-26 21:48:49 +0800

    20140526

--- sub_countingsort_for_hash.pl
+++ sub_countingsort_for_hash.pl
@@ -4,8 +4,8 @@
 	"b" =>1,
 	"c" =>2,
 	"d" =>1,
-	"e" =>1,
-	"f" =>1,
+	"e" =>5,
+	"f" =>7,
 	"g" =>1,
 	"h" =>2,
 	'aaa$a#b!'=>3,
@@ -17,27 +17,22 @@ print "$_ " for (@out);
 sub cs {
 	my $in_ref = shift;
 	my @in = %$in_ref;
-	my %hash = ();
-	my @AUX = ();
+	my %hash;
+	my @AUX;
 	for($i = 0; $i < $#in; $i = $i + 2){
-		my $reg = $in[$i+1];
-		push @{$hash{$reg}}, $in[$i];
+		push @{$hash{$in[$i+1]}}, $in[$i];
 	}
 	for my $num (keys %hash){
-		my $i = 0;
-		for(@{$hash{$num}}){ $i++; }
+		my $i = @{$hash{$num}};
 		@{$hash{$num}} = sort (@{$hash{$num}});
 		if ($i < 2){ $AUX[$num]++;  } else { $AUX[$num] += $i; }
 	}
 	for my $i (1..$#AUX){ $AUX[$i] += $AUX[$i-1] ; }
 	for my $i (keys %hash){
 		for my $value (@{$hash{$i}}){
-			my $reg = $AUX[$i] - 1;
-			$sort[$reg] = $value;
+			$sort[$AUX[$i]-1] = $value;
 			$AUX[$i]--;
-			print "reg=$reg\n";
 		}
 	}
-	# shift @sort;
 	return reverse @sort;
 }
