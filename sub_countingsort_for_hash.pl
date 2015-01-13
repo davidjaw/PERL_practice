c39b70c8f1f6ea6ec45bb91c6ec45a1223312e11
David Jaw <davidjaw@users.noreply.github.com>
2015-01-14 00:04:34 +0800

    update!

--- sub_countingsort_for_hash.pl
+++ sub_countingsort_for_hash.pl
@@ -3,23 +3,20 @@ use v5.18.2;
 sub cs {
 	my $in_ref = shift;
 	my @in = %$in_ref;
-	my %hash;
-	my @AUX;
-	my @sort;
+	my ($hash, @C, @sort);
 	for(my $i = 0; $i < $#in; $i = $i + 2){
-		push @{$hash{$in[$i+1]}}, $in[$i];
+		push @{$hash->{$in[$i+1]}}, $in[$i];
 	}
-	for my $num (keys %hash){
-		my $i = @{$hash{$num}};
-		@{$hash{$num}} = sort (@{$hash{$num}});
-		if ($i < 2){ $AUX[$num]++;  } else { $AUX[$num] += $i; }
+	for my $num (keys %$hash){
+		my $i = @{$hash->{$num}};
+		if ($i < 2){ $C[$num]++;  } else { $C[$num] = $i; }
 	}
-	for my $i (1..$#AUX){ $AUX[$i] += $AUX[$i-1] ; }
-	for my $i (keys %hash){
-		for my $value (@{$hash{$i}}){
-			$sort[$AUX[$i]-1] = $value;
-			$AUX[$i]--;
+	for my $i (1..$#C){ $C[$i] += $C[$i-1] ; }
+	for my $num (keys %$hash){
+		for my $value (@{$hash->{$num}}){
+			$sort[$C[$num]-1] = $value;
+			$C[$num]--;
 		}
 	}
-	return reverse @sort;
+	return @sort;
 }
